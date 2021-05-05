20a63773ffe67a391842a6186d29d67055e2d96a
VibhoreSagar <vibhoresagar@csus.edu>
2021-05-04 17:18:38 -0700

    Changes

--- Java-Projects/Final Project /FinalProject/src/SpellGame.java
+++
@@ -1,8 +0,0 @@
-class SpellGame {
-	public static void main(String[] args) {
-		// initialize the frame and set it to visible
-		GameFrame game = new GameFrame();
-		game.pack();
-		game.setVisible(true);
-	}
-}
